3cd35db36977f96419b8bf8c510ff67bdd94dfa5
Rene Ibanez <renedibanez17@gmail.com>
2018-05-09 20:31:40 -0400

    working out bugs in my problem

---
+++ chapter-3/js/following-second.js
@@ -0,0 +1,8 @@
+let userHour = Number(prompt("give me the Hour"))
+let userMin = Number(prompt("give me the Min"))
+let userSec = Number(prompt("give me the Sec"))
+
+
+if( userSec == 59 ){
+  console.log(usermin ++, && userSec = 00);
+}
