97221690b4f09344f55df4b56cb0c60aeb22f7ba
Rene Ibanez <renedibanez17@gmail.com>
2018-06-25 23:58:41 -0400

    still working towards solving starting List of words in chapter-7

--- chapter-7/js/list-of-words.js
+++ chapter-7/js/list-of-words.js
@@ -1,4 +1,13 @@
 // Write a program that asks the user for a word until the user types "stop".
 // The program then shows each of these words, except "stop".
 
-const 
+let userWord = prompt("stops taking word as soon as you type in 'stop'");
+const stop = "stop";
+let array = [];
+
+while (userWord != stop ) {
+  array.push(userWord)
+   userWord = prompt("stops taking word as soon as you type in 'stop'");
+
+}
+console.log(array.join(", "));
