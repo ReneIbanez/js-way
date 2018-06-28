f9cd55213c92cb3811bac5c961b34f579400918f
Rene Ibanez <renedibanez17@gmail.com>
2018-06-28 17:57:41 -0400

    workiing on chapter-8 completed word-ifo

--- chapter-8/js/word-info.js
+++ chapter-8/js/word-info.js
@@ -1,1 +1,12 @@
-alert("word-info.js")
+// Write a program that asks you for a word then shows its length,
+// lowercase, and uppercase values.
+
+let userWord = prompt("give me a word and get the length");
+let caps = userWord.toUpperCase()
+let lowercase = userWord.toLowerCase()
+
+let length = userWord.length
+
+console.log(length);
+console.log(caps);
+console.log(lowercase);
